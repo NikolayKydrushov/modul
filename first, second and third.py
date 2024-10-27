2171ad22f54034990c20bd98b3fbe6ead57e3613
Kolya <nnikolay2909@gmail.com>
2024-10-27 18:59:54 +0300

    2 задание, 2

--- first, second and third.py
+++ first, second and third.py
@@ -1,6 +1,12 @@
-first = input()
-second = input()
-third = input()
+first = input("Введите первое число: ")
+second = input("Введите второе число: ")
+third = input("Введите третье число: ")
 
-if first != second and first != third and second!= third:
+if first != second and first != third and second != third:
     print(0)
+elif first == second and first == third:
+    print(2)
+elif first == second or first == third or second == third:
+    print(3)
+#else:
+#    print(0)
