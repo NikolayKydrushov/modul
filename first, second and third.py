37fbb507dc0f5f886b620322ff28d498ddf7e6e6
Kolya <nnikolay2909@gmail.com>
2024-10-27 19:04:57 +0300

    2 задание, окончательное решение

--- first, second and third.py
+++ first, second and third.py
@@ -4,9 +4,7 @@ third = input("Введите третье число: ")
 
 if first != second and first != third and second != third:
     print(0)
-elif first == second and first == third:
-    print(2)
-elif first == second or first == third or second == third:
+elif first == second and first == third and second == third:
     print(3)
-#else:
-#    print(0)
+elif first == second or first == third or second == third:
+    print(2)
