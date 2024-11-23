71cc9766c17ec444d42295f18986d7d710b572a0
Kolya <nnikolay2909@gmail.com>
2024-11-23 19:04:24 +0300

    исправленный вараинт

--- module_3_5.py
+++ module_3_5.py
@@ -1,5 +1,7 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    if not str_number:
+        return 1
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
@@ -7,3 +9,11 @@ def get_multiplied_digits(number):
         return first
 
 print(get_multiplied_digits(40203))
+
+
+#Здравствуйте!
+#Задание выполнено почти верно, однако есть небольшая ошибка:
+#1) При умножении цифр числа нули должны быть учтены:
+# при встрече с 0 функция должна продолжать рекурсию, пропуская этот ноль,
+# так как умножение на 0 должно обнулять результат.
+# В текущем виде функция не обнуляет результат при наличии нуля.
