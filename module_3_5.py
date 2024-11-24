a8af26360e40a33df57f4dfe5c800898eba200df
Kolya <nnikolay2909@gmail.com>
2024-11-24 18:35:58 +0300

    вараинт

--- module_3_5.py
+++ module_3_5.py
@@ -1,14 +1,22 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+
     if not str_number:
         return 1
+
+    if len(str_number) == 1:
+        if int(str_number) == 0:
+            return 1
+        else:
+            return int(str_number)
+
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
-print(get_multiplied_digits(40203))
+print(get_multiplied_digits(420))
 
 
 #Здравствуйте!
